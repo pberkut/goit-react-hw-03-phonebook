67df0fa4aa047fb3d6cecab75b89556f8c7c6d79
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-02-18 15:22:42 +0200

    update

---
+++ src/components/Wrapper/Wrapper.jsx
@@ -0,0 +1,3 @@
+export const Wrapper = ({ children }) => {
+  return <div>{children}</div>;
+};
---
+++ src/components/Wrapper/Wrapper.styled.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  margin-top: 16px;
+  margin-bottom: 16px;
+`;
