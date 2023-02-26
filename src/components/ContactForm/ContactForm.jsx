3ed84dc99cc7563a6094891ebc2724a686f39651
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-02-26 15:19:32 +0200

    update

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -4,6 +4,11 @@ import * as Yup from 'yup';
 import { ErrorMessage, FormField, Field, Button } from './ContactForm.styled';
 import { nanoid } from 'nanoid';
 
+const INITIAL_VALUE = {
+  name: '',
+  number: '',
+};
+
 const ContactFormSchema = Yup.object().shape({
   name: Yup.string()
     .min(3, 'Must be 3 characters or more')
@@ -15,50 +20,49 @@ const ContactFormSchema = Yup.object().shape({
     .required('Required'),
 });
 
-const INITIAL_VALUE = {
-  name: '',
-  number: '',
-};
+export const ContactForm = ({ onSave }) => {
+  const handleSubmit = (values, actions) => {
+    onSave({
+      ...values,
+      id: nanoid(2),
+    });
+    actions.resetForm();
+  };
 
-export const ContactForm = ({ onSave }) => (
-  <Formik
-    initialValues={{ ...INITIAL_VALUE }}
-    validationSchema={ContactFormSchema}
-    onSubmit={(values, actions) => {
-      onSave({
-        ...values,
-        id: nanoid(2),
-      });
-      actions.resetForm();
-    }}
-  >
-    <Form>
-      <FormField>
-        Name
-        <Field
-          name="name"
-          type="text"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
-        <ErrorMessage name="name" component="p" />
-      </FormField>
-      <FormField>
-        Number
-        <Field
-          name="number"
-          type="tel"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-        />
-        <ErrorMessage name="number" component="p" />
-      </FormField>
-      <Button type="submit">Add contact</Button>
-    </Form>
-  </Formik>
-);
+  return (
+    <Formik
+      initialValues={INITIAL_VALUE}
+      validationSchema={ContactFormSchema}
+      onSubmit={handleSubmit}
+    >
+      <Form>
+        <FormField>
+          Name
+          <Field
+            name="name"
+            type="text"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+          <ErrorMessage name="name" component="p" />
+        </FormField>
+        <FormField>
+          Number
+          <Field
+            name="number"
+            type="tel"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+          <ErrorMessage name="number" component="p" />
+        </FormField>
+        <Button type="submit">Add contact</Button>
+      </Form>
+    </Formik>
+  );
+};
 
 ContactForm.propTypes = {
   onSave: PropTypes.func.isRequired,
